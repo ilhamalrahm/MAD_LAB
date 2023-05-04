b08549f6243d26ec0bbbee11b8af34271ae5ac0b
Ilham-Rahm <ilhamalrahm@gmail.com>
2023-05-04 11:02:07 +0530

    End sem practice

---
+++ Mix/app/src/main/java/com/example/mix/Homepage.java
@@ -0,0 +1,100 @@
+package com.example.mix;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+public class Homepage extends AppCompatActivity {
+
+    SQLiteDatabase db=SQLiteDatabase.openOrCreateDatabase("/data/data/com.example.mix/database/mix.db",null,null);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        Cursor cursor=db.rawQuery("Select name from User",null);
+        cursor.moveToLast();
+        String name=cursor.getString(0);
+        cursor.close();
+        TextView n=(TextView)findViewById(R.id.Welcome);
+        String welcomeMes="Welcome "+ name;
+
+        n.setText(welcomeMes);
+        Button b = (Button) findViewById(R.id.button2);
+
+        registerForContextMenu(b);
+
+
+//        PopupMenu p=new PopupMenu(Homepage.this,b);
+//        Toolbar t=(Toolbar)findViewById(R.id.toolbar3) ;
+
+
+
+
+
+//        p.getMenuInflater().inflate(R.menu.popup,p.getMenu());
+
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                p.show();
+//            }
+//        });
+
+
+
+//        MenuInflater menuInflater=getMenuInflater();
+//        menuInflater.inflate(R.menu.popup,p);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.popup, menu);
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.cont1:
+//                Cursor cursor= db.rawQuery("delete from User where n",);
+                Cursor cursor=db.rawQuery("Select name from User",null);
+                cursor.moveToFirst();
+                String name=cursor.getString(0);
+                String[] names={name};
+                db.delete("User","name=?",names);
+
+                return true;
+            case R.id.cont2:
+//                deleteNote(info.id);
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+}
---
+++ Mix/app/src/main/java/com/example/mix/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.mix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edittext;
+    RadioButton male;
+    RadioButton female;
+    CheckBox wcc;
+    CheckBox mad;
+    Switch sw;
+    Spinner country;
+    Button submit;
+
+    String name;
+    String gender;
+    ArrayList<String> subjects=new ArrayList<String>();
+    String beastness="Lame";
+    String nation;
+    SQLiteDatabase db=SQLiteDatabase.openOrCreateDatabase("/data/data/com.example.mix/database/mix.db",null,null);
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String CreateTableQuery="Create table if not exists User(name VARCHAR,gender VARCHAR,beastness VARCHAR,nation VARCHAR)";
+        db.execSQL(CreateTableQuery);
+        String[] countries ={"India","Usa","Australia"};
+
+        country=(Spinner)findViewById(R.id.country);
+        submit=(Button)findViewById(R.id.button);
+        sw=(Switch)findViewById(R.id.switch1) ;
+        edittext=(EditText)findViewById(R.id.name);
+        wcc=(CheckBox)findViewById(R.id.wcc);
+        mad=(CheckBox)findViewById(R.id.MAD);
+        male=(RadioButton)findViewById(R.id.Male);
+        female=(RadioButton)findViewById(R.id.Female);
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                female.setChecked(false);
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                male.setChecked(false);
+            }
+        });
+
+
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,countries);
+        country.setAdapter(adapter);
+
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               name= edittext.getText().toString();
+               if(wcc.isChecked())
+               {
+                   subjects.add("WCC");
+               }
+                if(mad.isChecked())
+                {
+                    subjects.add("MAD");
+                }
+                if(sw.isChecked())
+                {
+                    beastness="Beast Mode";
+                }
+                else {
+                   beastness= "Lame";
+                }
+                gender=male.isChecked()?"Male":"Female";
+                nation=country.getSelectedItem().toString();
+
+
+
+                System.out.println(name);
+                System.out.println(beastness);
+                System.out.println(nation);
+                System.out.println(gender);
+                ContentValues values=new ContentValues();
+
+                values.put("name",name);
+                values.put("gender",gender);
+                values.put("beastness",beastness);
+                values.put("nation",nation);
+
+                db.insert("User",null,values);
+
+                Intent i=new Intent(MainActivity.this,Homepage.class);
+                startActivity(i);
+
+
+
+
+            }
+        });
+
+    }
+}
